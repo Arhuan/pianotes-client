d700567d98beab709e350ef8b3a42217587eb0d3
Arhuan <aaronhuangwork@gmail.com>
2020-12-25 17:05:05 -0800

    added cursor styles

--- src/components/staff/StaffComponent.jsx
+++ src/components/staff/StaffComponent.jsx
@@ -4,81 +4,111 @@ import { Col, Row } from 'antd';
 
 import './StaffComponent.css';
 
+// eslint-disable-next-line no-unused-vars
+const TREBLE_NOTES = [
+  ['G'],
+  ['F'],
+  ['E'],
+  ['D'],
+  ['C'],
+  ['B'],
+  ['A'],
+  ['G'],
+  ['F'],
+  ['E'],
+  ['D'],
+];
+
+// eslint-disable-next-line no-unused-vars
+const BASS_NOTES = [
+  ['B'],
+  ['A'],
+  ['G'],
+  ['F'],
+  ['E'],
+  ['D'],
+  ['C'],
+  ['B'],
+  ['A'],
+  ['G'],
+  ['F'],
+];
+
 function StaffComponent() {
   return (
     <div className="staff-notes">
       <Row>
-        <Col className="notes-col">O</Col>
-        <Col className="notes-col">O</Col>
-        <Col className="notes-col">O</Col>
-        <Col className="notes-col">O</Col>
-        <Col className="notes-col">O</Col>
-      </Row>
-      <Row className="strikethrough">
-        <Col className="notes-col">O</Col>
-        <Col className="notes-col">O</Col>
-        <Col className="notes-col">O</Col>
-        <Col className="notes-col">O</Col>
-        <Col className="notes-col">O</Col>
-      </Row>
-      <Row>
-        <Col className="notes-col">O</Col>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
       </Row>
       <Row className="strikethrough">
-        <Col className="notes-col">O</Col>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
       </Row>
       <Row>
-        <Col className="notes-col">O</Col>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
       </Row>
       <Row className="strikethrough">
-        <Col className="notes-col">O</Col>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
       </Row>
       <Row>
-        <Col className="notes-col">O</Col>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
       </Row>
       <Row className="strikethrough">
-        <Col className="notes-col">O</Col>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
       </Row>
       <Row>
-        <Col className="notes-col">O</Col>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
       </Row>
       <Row className="strikethrough">
-        <Col className="notes-col">O</Col>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
       </Row>
       <Row>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
+        <Col className="notes-col">O</Col>
+        <Col className="notes-col">O</Col>
+        <Col className="notes-col">O</Col>
+      </Row>
+      <Row className="strikethrough">
+        <Col className="notes-col cursor">O</Col>
+        <Col className="notes-col">O</Col>
+        <Col className="notes-col">O</Col>
+        <Col className="notes-col">O</Col>
+        <Col className="notes-col">O</Col>
+      </Row>
+      <Row>
+        <Col className="notes-col cursor">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
         <Col className="notes-col">O</Col>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,7 +5,6 @@ import './HomePage.css';
 function HomePage() {
   return (
     <div className="home-page">
-      a tool to help you memorize musical notes for the piano
       <div className="notes-component">
         <NotesComponent />
       </div>
