4a28849ead776a931f7b7825c571de50683078fa
Arhuan <aaronhuangwork@gmail.com>
2021-04-30 23:51:37 -0700

    add contact info to footer

--- src/components/footer/FooterComponent.jsx
+++ src/components/footer/FooterComponent.jsx
@@ -1,21 +1,67 @@
 import React from 'react';
 
 import { Footer } from 'antd/lib/layout/layout';
+import {
+  GithubOutlined,
+  LinkedinOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
 
 import './FooterComponent.css';
 
 function FooterComponent() {
   return (
     <Footer className="footer-container">
-      <div>
-        Icons made by{' '}
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-          Freepik
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
+      <div className="footer-content">
+        <div>
+          Icons made by{' '}
+          <a
+            href="https://www.flaticon.com/authors/freepik"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Freepik"
+          >
+            Freepik
+          </a>{' '}
+          from{' '}
+          <a
+            href="https://www.flaticon.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Flaticon"
+          >
+            www.flaticon.com
+          </a>
+        </div>
+        <div className="footer-contact">
+          <a
+            href="https://github.com/Arhuan"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <div className="contact-item">
+              <GithubOutlined className="footer-icon" />
+            </div>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/aahuangg/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <div className="contact-item">
+              <LinkedinOutlined className="footer-icon" />
+            </div>
+          </a>
+          <a
+            href="mailto:aaronhuangwork@gmail.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <div className="contact-item">
+              <MailOutlined className="footer-icon" />
+            </div>
+          </a>
+        </div>
       </div>
     </Footer>
   );
